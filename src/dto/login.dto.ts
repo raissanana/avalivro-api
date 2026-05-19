20b28866844fc3694027b0ff0d82efc16aadc59e
raissanana <raissalimmaa@gmail.com>
2026-05-19 16:05:10 -0300

    deploy

--- src/dto/login.dto.ts
+++ src/dto/login.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 export class LoginDTO {
-    email!: string;
-    senha!: string;
+    @IsNotEmpty({ message: 'Email é obrigatório' })
+    @IsString({ message: 'Email deve ser uma string' })
+    email: string;
+
+    @IsNotEmpty({ message: 'Senha é obrigatória' })
+    @IsString({ message: 'Senha deve ser uma string' })
+    senha: string;
 }
